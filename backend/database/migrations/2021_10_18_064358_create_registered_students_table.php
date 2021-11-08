4d6c3107ddfe5212a673726d8463d6e51c6dcea3
_hi_vietthang <61310039+vthcvn@users.noreply.github.com>
2021-11-08 10:47:57 +0700

    update migrate

--- backend/database/migrations/2021_10_18_064358_create_registered_students_table.php
+++ backend/database/migrations/2021_10_18_064358_create_registered_students_table.php
@@ -17,7 +17,7 @@ class CreateRegisteredStudentsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             // $table->foreignId('user_id')->constrained();
-            $table->foreignId('course_id')->constrained();
+            // $table->foreignId('course_id')->constrained();
             $table->foreignId('course_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
